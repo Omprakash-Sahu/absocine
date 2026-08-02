549b6670135951a0c31c8248d08128b5217a4bf3
Omprakash Sahu <152634318+Omprakash-Sahu@users.noreply.github.com>
2026-08-02 08:25:45 +0530

    Add react router functionality.
    - Made a dedicated pages/HomePage.jsx file for the home page content.
    - App.jsx is modified to take routing decisions.
    - Decided to keep header visible on all pages. So, Header component stays in App.jsx & not in HomePage.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
 import Header from './components/Header';
-import SearchBox from './components/SearchBox';
-import BannerContainer from './components/BannerContainer';
-import MovieSection from './components/MovieSection';
-import useFetchMovies from './hooks/useFetchMovies';
-
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const trendingUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
-const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
-const topRatedUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
-const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
-const upcomingUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`;
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
 
-  const { moviesList:trendingMovies, loading:loadingTrendingMovies } = useFetchMovies(trendingUrl);
-  const { moviesList:popularMovies, loading:loadingPopularMovies } = useFetchMovies(popularUrl);
-  const { moviesList:topRatedMovies, loading:loadingTopRatedMovies } = useFetchMovies(topRatedUrl);
-  const { moviesList:nowPlayingMovies, loading:loadingNowPlayingMovies } = useFetchMovies(nowPlayingUrl);
-  const { moviesList:upcomingMovies, loading:loadingUpcomingMovies } = useFetchMovies(upcomingUrl);
-
-  if (loadingTrendingMovies || loadingPopularMovies || loadingTopRatedMovies || loadingNowPlayingMovies || loadingUpcomingMovies) 
-    return (
-      <div className='bg-red-500 text-white text-center'>Loading...</div>
-    )
-  
   return (
-    <div className='bg-background flex flex-col'>
+    <>
       <Header />
-      <BannerContainer movie={trendingMovies[0]}/>
-      <MovieSection title="Now Playing" moviesList={nowPlayingMovies} />
-      <MovieSection title="Trending" moviesList={trendingMovies} />
-      <MovieSection title="Popular" moviesList={popularMovies} />
-      <MovieSection title="Top Rated" moviesList={topRatedMovies} />
-      <MovieSection title="Upcoming" moviesList={upcomingMovies} />
-    </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>    
+    </>
   )
 
 }
